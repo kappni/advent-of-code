7f8b1d1a548b8fd37df18aad7c710e406b562459
Nico Kappeler <53252190+kappni@users.noreply.github.com>
2022-12-04 23:54:59 +0100

    fix: update Program.cs to correct version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿IDay day = new Day4();
+
+string result1 = await day.Execute1();
+string result2 = await day.Execute2();
+
+Console.WriteLine($"result 1: {result1}, result2: {result2}");
